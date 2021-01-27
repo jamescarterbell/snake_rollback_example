35c07a231ce32d9ce6b720e077d34cf736e219a0
James <jamescarterbell@gmail.com>
2021-01-27 17:58:00 -0500

    Using immediate_rendering, almost working

--- src/snake_visuals.rs
+++ src/snake_visuals.rs
@@ -1,15 +1,11 @@
 use bevy::prelude::*;
 use bevy::render::pass::ClearColor;
-use bevy::render::draw::{DrawContext, DrawError};
-use bevy::render::pipeline::{PipelineSpecialization, PipelineDescriptor, VertexBufferDescriptor, InputStepMode, VertexAttributeDescriptor, VertexFormat };
-use bevy::render::mesh;
-use bevy::render::renderer::{BindGroup, RenderResourceBindings, RenderResourceId};
+use bevy::render::camera::Camera;
+use bevy::render::texture::SamplerDescriptor;
 use std::collections::HashMap;
 use bevy_rollback::LQuery;
 use crate::{MoveDirection, SnakeHead, Player};
-use bevy::sprite::{QUAD_HANDLE, SPRITE_PIPELINE_HANDLE, SPRITE_SHEET_PIPELINE_HANDLE};
-use std::borrow::Cow;
-use std::ops::Range;
+use bevy_immediate_rendering::SpriteAtlasDrawer;
 
 pub mod stage{
     pub const LOAD_ASSETS: &str = "load_assets";
@@ -18,6 +14,7 @@ pub mod stage{
 pub struct SnakeSpriteHandles{
     pub texture: Handle<TextureAtlas>,
     pub sprites: HashMap<SnakeSprites, u32>,
+    pub sampler: SamplerDescriptor,
 }
 
 #[derive(Eq, PartialEq, Hash, Ord, PartialOrd, Clone, Debug)]
@@ -60,7 +57,7 @@ impl Plugin for SnakeVisualsPlugin{
 fn setup(
     commands: &mut Commands, 
     asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ){
 
     let texture_handle = asset_server.load(r#"sprites.png"#);
@@ -86,135 +83,22 @@ fn setup(
                     map.insert(SnakeSprites::RedTail, 12);
                     map.insert(SnakeSprites::BlueTail, 13);
                     map
-                }
+                },
+                sampler: SamplerDescriptor::default(),
             }
         )
-        .spawn(Camera2dBundle::default());    
+        .spawn(Camera2dBundle::default());  
 }
 
 fn draw_snake(
-    mut context: DrawContext,
     characters: LQuery<(&Transform, &SnakeHead, &Player, &MoveDirection)>,
+    cam: Query<(&Camera, &Transform)>,
     sprite_handles: Res<SnakeSpriteHandles>,
-    texture_atlases: Res<Assets<TextureAtlas>>){
-
-    let mut drawer = SpriteAtlasDrawer::new(&mut context, 1);
-
-    for (transform, head, player, dir) in characters.iter(){
-        drawer.draw_sprite_atlas(&transform, &sprite_handles.texture, *sprite_handles.sprites.get(&SnakeSprites::RedHead).unwrap(), Color::rgb(1.0, 1.0, 1.0));
-    }
-}
-
-struct SpriteAtlasDrawer<'a, 'b>{
-    draw: Draw,
-    draw_context: &'b mut DrawContext<'a>
-}
-
-impl<'b, 'a> SpriteAtlasDrawer<'a, 'b>{
-    pub fn new(draw_context: &'b mut DrawContext<'a>, msaa_samples: u32) -> Self{
-        let mut draw = Draw::default();
-        draw_context.set_pipeline(
-            &mut draw,
-            &SPRITE_SHEET_PIPELINE_HANDLE.typed(),
-            &PipelineSpecialization{
-                sample_count: msaa_samples,
-                vertex_buffer_descriptor: VertexBufferDescriptor{
-                    name: Cow::Borrowed("sprite_texture"),
-                    stride: std::mem::size_of::<f32>() as u64 * 8,
-                    step_mode: InputStepMode::Vertex,
-                    attributes: vec![
-                        VertexAttributeDescriptor{
-                            name: Cow::Borrowed("Vertex_Position"),
-                            offset: 0,
-                            shader_location: 0,
-                            format: VertexFormat::Float3
-                        },
-                        VertexAttributeDescriptor{
-                            name: Cow::Borrowed("Vertex_Normal"),
-                            offset: std::mem::size_of::<f32>() as u64 * 3,
-                            shader_location: 1,
-                            format: VertexFormat::Float3
-                        },
-                        VertexAttributeDescriptor{
-                            name: Cow::Borrowed("Vertex_Uv"),
-                            offset: std::mem::size_of::<f32>() as u64 * 2,
-                            shader_location: 2,
-                            format: VertexFormat::Float3
-                        },
-                    ]
-                },
-                ..Default::default()
-            }
-        );
-
-        Self{
-            draw_context,
-            draw,
+    mut sprite_atlas_drawer: SpriteAtlasDrawer,
+){
+    for (cam, cam_transform) in cam.iter(){
+        for (transform, head, player, dir) in characters.iter(){
+            sprite_atlas_drawer.draw(transform, cam_transform, cam, &sprite_handles.texture, *sprite_handles.sprites.get(&SnakeSprites::RedHead).unwrap(), Color::rgb(1.0, 1.0, 1.0), &sprite_handles.sampler);
         }
-    }
-
-    fn set_mesh_attributes(&mut self) -> Result<Range<u32>, DrawError>{
-        let render_resource_context = &**self.draw_context.render_resource_context;
-
-        if let Some(RenderResourceId::Buffer(vertex_attribute_buffer_id)) = render_resource_context
-            .get_asset_resource(
-                &QUAD_HANDLE.typed::<Mesh>(),
-                mesh::VERTEX_ATTRIBUTE_BUFFER_ID,
-            )
-        {
-            self.draw.set_vertex_buffer(0, vertex_attribute_buffer_id, 0);
-        } else {
-            println!("Could not find vertex buffer for `bevy_sprite::QUAD_HANDLE`.")
-        }
-
-        let mut indices = 0..0;
-        if let Some(RenderResourceId::Buffer(quad_index_buffer)) = render_resource_context
-            .get_asset_resource(
-                &QUAD_HANDLE.typed::<Mesh>(),
-                mesh::INDEX_BUFFER_ASSET_INDEX,
-            )
-        {
-            self.draw.set_index_buffer(quad_index_buffer, 0);
-            if let Some(buffer_info) = render_resource_context.get_buffer_info(quad_index_buffer) {
-                indices = 0..(buffer_info.size / 4) as u32;
-            } else {
-                panic!("Expected buffer type.");
-            }
-        }
-
-        // set global bindings
-        self
-            .draw_context
-            .set_bind_groups_from_bindings(
-                &mut self.draw, 
-                &mut [])?;
-        Ok(indices)
-    }
-
-    pub fn draw_sprite_atlas(&mut self, transform: &Transform, atlas: &Handle<TextureAtlas>, index: u32, color: Color) -> Result<(), DrawError>{
-        let indices = self.set_mesh_attributes()?;
-        self.draw_context.set_asset_bind_groups(&mut self.draw, atlas)?;
-        
-        let sprite = TextureAtlasSprite{
-            index,
-            color
-        };
-
-        let transform = transform.compute_matrix();
-        let transform_buffer = self.draw_context.get_uniform_buffer(&transform).unwrap();
-        let sprite_buffer = self.draw_context.get_uniform_buffer(&sprite).unwrap();
-        let sprite_bind_group = BindGroup::build()
-            .add_binding(0, transform_buffer)
-            .add_binding(1, sprite_buffer)
-            .finish();
-        self.draw_context.create_bind_group_resource(2, &sprite_bind_group)?;
-        self.draw.set_bind_group(2, &sprite_bind_group);
-        self.draw.draw_indexed(indices.clone(), 0, 0..1);
-        Ok(())
-    }
-    
-    pub fn draw_sprite_atlas_instanced(&mut self, transform: &[Transform], atlas: &Handle<TextureAtlas>, indicies: &[u32]){
-        self.draw_context.set_asset_bind_groups(&mut self.draw, atlas);
-        todo!()
     }
 }
